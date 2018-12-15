dff0f4d43f1cb17b3e84c33158e54456a7c17e55
ItaLestari11 <45660967+ItaLestari11@users.noreply.github.com>
2018-12-15 16:07:17 +0700

    Add files via upload

---
+++ Bilangan Ganjil dan Genap.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int bilangan;
+
+cout<<"Program menentukan bilangan ganjil genap"<<endl;
+cout<<"ITA LESTARI"<<endl;
+cout<<"311810183"<<endl;
+cout<<"masukan bilangan"<<endl;
+cin>>bilangan;
+if(bilangan%2==0)
+    cout<<"bilangan genap"<<endl;
+else
+    cout<<"bilangan ganjil"<<endl;
+return 0;
+}
